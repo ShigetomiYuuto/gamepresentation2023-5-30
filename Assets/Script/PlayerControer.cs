b7d92615bf638054045df81ac03cc6e70308cbb9
Octorok192 <awmcco893@i.softbank.jp>
2023-06-09 18:10:37 +0900

    A,Dkeyでの横移動を追加

---
+++ Assets/Script/PlayerControer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using static UnityEngine.EventSystems.StandaloneInputModule;
+using UnityEngine.InputSystem;
+
+public class PlayerControer : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField] private float _moveSpeed = 3;
+    private Vector2 _moveInput;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        var current = Keyboard.current;
+
+        var aKey = current.aKey;
+
+        var dKey = current.dKey;
+
+        if (aKey.isPressed)
+        {
+            _rb.AddForce(Vector2.left * _moveSpeed, ForceMode2D.Force);
+        }
+        if (dKey.isPressed)
+        {
+            _rb.AddForce(Vector2.right * _moveSpeed, ForceMode2D.Force);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        _rb.velocity = new Vector2(_moveInput.x * _moveSpeed,_rb.velocity.y);
+    }
+}
