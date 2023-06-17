e7fd8ff0ce3038c28f9c1f8bb52ff20492608bbe
Octorok192 <awmcco893@i.softbank.jp>
2023-06-18 00:06:29 +0900

    プレイヤーが死んだときに最初から始まるようになる仕様にしました。 特定のポイントに触れたら次にステージに移行するようにしました。 tilemapを使えるようにしました。

---
+++ Assets/Script/Replayplayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replayplayer : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("ƒvƒŒƒCƒ„[‚Ìprefab‚ğ’Ç‰Á")] private GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject _playerob = GameObject.Find(playerPrefab.name);
+
+        //playerob‚ª‘¶İ‚µ‚Ä‚¢‚È‚¢ê‡
+        if (_playerob == null )
+        {
+            GameObject _newPlayerob= Instantiate(playerPrefab);
+            _newPlayerob.name = playerPrefab.name;
+        }
+    }
+}
